957358ea7c8701ed7cc8deeb419076a3570b837b
cheddar <cheddar@users.noreply.github.com>
2014-08-26 12:24:04 -0500

    Update start.sh to default SERVE_STATIC

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -eu
 
+export SERVE_STATIC=dist 
+
 . config/env.sh
 npm run build-config
-exec node --trace_gc --max_new_space_size=16384 --max_old_space_size=128 app.js
+exec node --trace_gc --max_old_space_size=128 app.js
