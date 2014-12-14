ba1ddf61e4791283c966dd9794c69949ae4e6809
kentquirk <kentquirk@gmail.com>
2014-12-13 22:42:21 -0500

    Fix isNumber and split up the failing test

--- test/schema/wizardTest.js
+++ test/schema/wizardTest.js
@@ -1,15 +1,15 @@
 /*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -232,14 +232,18 @@ describe('schema/wizard.js', function(){
     helper.rejectIfAbsent(goodObject, 'recommended');
     helper.expectObjectField(goodObject, 'recommended');
 
-    it.skip('contains only numerical fields', function(done){
+    it.skip('carb is a numeric field', function(done){
       var obj = _.cloneDeep(goodObject);
       obj.recommended.carb = '1';
       helper.expectRejection(obj, 'recommended', done);
-      obj.recommended.carb = goodObject.recommended.carb;
+    });
+    it.skip('correction is a numeric field', function(done){
+      var obj = _.cloneDeep(goodObject);
       obj.recommended.correction = '2';
       helper.expectRejection(obj, 'recommended', done);
-      obj.recommended.correction = goodObject.recommended.correction;
+    });
+    it.skip('net is a numeric field', function(done){
+      var obj = _.cloneDeep(goodObject);
       obj.recommended.net = '3';
       helper.expectRejection(obj, 'recommended', done);
     });
