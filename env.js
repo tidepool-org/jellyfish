88b3b9c63ea686c5d9f012248221501845b155ae
ianjorgensen <jorgensen.ian@gmail.com>
2014-12-18 12:14:39 -0200

    add gatekeeper config, common to all repos

--- env.js
+++ env.js
@@ -1,15 +1,15 @@
 /*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -63,6 +63,11 @@ module.exports = (function () {
 
     // The secret to use when getting a server token from user-api
     serverSecret: config.fromEnvironment("SERVER_SECRET")
+  };
+
+  env.gatekeeper = {
+    // The config object to discover gatekeeper.  This is just passed through to hakken.watchFromConfig()
+    serviceSpec: JSON.parse(config.fromEnvironment('GATEKEEPER_SERVICE'))
   };
 
   /**
