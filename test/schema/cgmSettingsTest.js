9735e719f7153d2e3a0163690b27ef27ad8a258c
Jamie <jamie@tidepool.org>
2015-07-02 20:57:23 +1200

    start of cgmSettings tests

---
+++ test/schema/cgmSettingsTest.js
@@ -0,0 +1,117 @@
+/*
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
+ /* global describe, before, beforeEach, it, after */
+
+'use strict';
+
+var _ = require('lodash');
+var expect = require('salinity').expect;
+
+var helper = require('./schemaTestHelper.js');
+
+var goodObject = {
+  type: 'cgmSettings',
+  time: '2015-06-16T13:09:10.000Z',
+  timezoneOffset: 120,
+  deviceId: 'test',
+  uploadId: 'test',
+  transmitterId: 'test',
+  units: 'mg/dL',
+  "highAlerts": {
+      "enabled": true,
+      "level": 8.3261219865683,
+      "snooze": 0
+  },
+  "lowAlerts": {
+      "enabled": true,
+      "level": 3.6079861941795968,
+      "snooze": 0
+  },
+  "outOfRangeAlerts": {
+      "enabled": false,
+      "snooze": 1800000
+  },
+  "rateOfChangeAlerts": {
+      "fallRate": {
+          "enabled": false,
+          "rate": -0.16652243973136602
+      },
+      "riseRate": {
+          "enabled": false,
+          "rate": 0.16652243973136602
+      }
+  },
+  _groupId: 'g'
+};
+
+/*
+{
+        "deviceId": "DexG4RecwitSha_SM51118608",
+        "deviceTime": "2015-06-16T09:09:10",
+        "guid": "0ba0f39d-6e21-4454-a6f0-ac2fb10c3a9b",
+        "highAlerts": {
+            "enabled": true,
+            "level": 8.3261219865683,
+            "snooze": 0
+        },
+        "id": "3t8f5h40i1245011ck8mtduledrftv2d",
+        "lowAlerts": {
+            "enabled": true,
+            "level": 3.6079861941795968,
+            "snooze": 0
+        },
+        "outOfRangeAlerts": {
+            "enabled": false,
+            "snooze": 1800000
+        },
+        "rateOfChangeAlerts": {
+            "fallRate": {
+                "enabled": false,
+                "rate": -0.16652243973136602
+            },
+            "riseRate": {
+                "enabled": false,
+                "rate": 0.16652243973136602
+            }
+        },
+        "time": "2015-06-16T13:09:10.000Z",
+        "timezoneOffset": -240,
+        "transmitterId": "68U6B",
+        "type": "cgmSettings",
+        "units": "mg/dL",
+        "uploadId": "upid_4d6253eba395"
+    }
+
+*/
+
+describe('schema/cgmSettings.js', function () {
+  describe('highAlerts', function () {
+    helper.rejectIfAbsent(goodObject, 'highAlerts');
+    helper.expectObjectField(goodObject, 'highAlerts');
+  });
+  describe('lowAlerts', function () {
+    helper.rejectIfAbsent(goodObject, 'lowAlerts');
+    helper.expectObjectField(goodObject, 'lowAlerts');
+  });
+  describe('rateOfChangeAlerts', function () {
+    helper.rejectIfAbsent(goodObject, 'rateOfChangeAlerts');
+    helper.expectObjectField(goodObject, 'rateOfChangeAlerts');
+  });
+
+  helper.testCommonFields(goodObject);
+});
