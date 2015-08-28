0599e6f932b28389338d7e2c66a8c43af39f3af9
Darin Krauss <darinkrauss@gmail.com>
2015-08-27 19:26:05 -0700

    Add README.md for rawUploads.js and update comments to match

---
+++ bin/README.md
@@ -0,0 +1,23 @@
+`rawUploads.js`
+
+The `rawUploads.js` script will list and download raw uploads for a given user.
+
+_Note: This script requires the environment be setup as if Jellyfish were
+running._
+
+There are two ways to use this script:
+
+1. To list all of the uploads for a given user (not the uploading user, but
+the target user):
+
+  `node bin/rawUploads.js list <user-id>`
+
+  This will list all uploads for the specified user id. The first field will be
+  the upload id and the second field will be the timestamp when the upload was
+  initiated.
+
+2. To download to stdout a particular upload by id:
+
+  `node bin/rawUploads.js download <upload-id>`
+
+  The unencrypted upload will be piped to stdout.
--- bin/rawUploads.js
+++ bin/rawUploads.js
@@ -16,25 +16,29 @@
  */
 
  /*
-  * Before running this script you need to setup your environment as if you
-  * were running Jellyfish.
+  * rawUploads.js
+  *
+  * The rawUploads.js script will list and download raw uploads for a given user.
+  *
+  * Note: This script requires the environment be setup as if Jellyfish were
+  * running.
   *
   * There are two ways to use this script:
   *
-  * 1) To list all of the uploads for a given user (not the uploading user, but
+  * 1. To list all of the uploads for a given user (not the uploading user, but
   * the target user):
   *
-  * node bin/rawUploads.js list <user-id>
+  *   node bin/rawUploads.js list <user-id>
   *
-  * This will list all uploads for the specified user id. The first field will
-  * be the upload id and the second field will be the timestamp when the upload
-  * was initiated.
+  *   This will list all uploads for the specified user id. The first field will be
+  *   the upload id and the second field will be the timestamp when the upload was
+  *   initiated.
   *
-  * 2) To download to stdout a particular upload by id:
+  * 2. To download to stdout a particular upload by id:
   *
-  * node bin/rawUploads.js download <upload-id>
+  *   node bin/rawUploads.js download <upload-id>
   *
-  * The unencrypted upload will be piped to stdout.
+  *   The unencrypted upload will be piped to stdout.
   */
 
 'use strict';
