df3c5a40051e7f287a635d0d48480f690d33c259
Jamie <jamie.h.bate@gmail.com>
2024-12-18 16:49:23 +1300

    initial load of QA3 test users

--- hash_platform_user_ids.sh
+++ hash_platform_user_ids.sh
@@ -6,8 +6,8 @@ USER_IDS_FILE=$2
 ## e.g. "qa3 hash"
 SALT_ITEM_NAME=$3
 
-SALT=$(op item get "$SALT_ITEM_NAME" --account tidepool.1password.com --fields label=credential --format json | jq -r '.value')
+SALT=$(op item get "$SALT_ITEM_NAME" --account tidepool.1password.com --fields label=password --format json | jq -r '.value')
 
-USER_IDS=$(jq -r '.[]' "$USER_IDS_FILE")
+USER_IDS=$(jq -r '.' "$USER_IDS_FILE")
 
-node -e "console.log(require('./lib/misc.js').addUserIdsToHashedEnvironmentFile('$ENV', '$SALT', '$USER_IDS'))"
+node -e "console.log(require('./lib/misc.js').addUserIdsToHashedEnvironmentFile('$ENV', '$SALT', $USER_IDS))"
